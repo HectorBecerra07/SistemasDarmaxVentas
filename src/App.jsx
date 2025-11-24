4610f7930627a1ec1623c7b75272fc6e64704363
Hector Gabriel Becerra Rivera <rivehect5t@gmail.com>
2025-11-24 13:52:34 -0600

    App.js modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// Importa tus componentes:
+import AguaPuraSeleccionTamano from "./SistemaDeVentas/compraDeGarrafones/AguaPuraSeleccionTamano";
+import AguaPuraPaso2AsignarGarrafones from "./SistemaDeVentas/compraDeGarrafones/AguaPuraPaso2AsignarGarrafones";
+import AguaPuraPaso3MetodoEntrega from "./SistemaDeVentas/compraDeGarrafones/AguaPuraPaso3MetodoEntrega";
+import AguaPuraResumenPago from "./SistemaDeVentas/compraDeGarrafones/AguaPuraResumenPago";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Define your routes here */}
-   
 
+        {/* Paso 1 */}
+        <Route
+          path="/comprar/seleccionar-tamano"
+          element={<AguaPuraSeleccionTamano />}
+        />
 
+        {/* Paso 2 */}
+        <Route
+          path="/comprar/asignar-garrafones"
+          element={<AguaPuraPaso2AsignarGarrafones />}
+        />
 
+        {/* Paso 3 */}
+        <Route
+          path="/comprar/metodo-entrega"
+          element={<AguaPuraPaso3MetodoEntrega />}
+        />
+
+        {/* Resumen y Pago */}
+        <Route
+          path="/comprar/resumen"
+          element={<AguaPuraResumenPago />}
+        />
+
+        {/* Inicio opcional */}
+        <Route path="/" element={<h1>Bienvenido a AguaPura</h1>} />
 
       </Routes>
     </BrowserRouter>
@@ -17,4 +44,3 @@ function App() {
 }
 
 export default App;
-
