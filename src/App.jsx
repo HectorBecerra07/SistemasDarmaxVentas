760bf64374bd1e7e9cda668eb2fc16f834842cd2
Hector Gabriel Becerra Rivera <rivehect5t@gmail.com>
2025-11-24 14:31:53 -0600

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import AguaPuraPaso2AsignarGarrafones from "./SistemaDeVentas/compraDeGarrafones/AguaPuraPaso2AsignarGarrafones.jsx";
 import AguaPuraSeleccionTamano from "./SistemaDeVentas/compraDeGarrafones/AguaPuraSeleccionTamano.jsx";
@@ -10,18 +10,16 @@ import Seleccion from "./SistemaDeVentas/Seleccion.jsx";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1>Bienvenido a AguaPura</h1>} />
-        <Route path="/comprar" element={<Seleccion />} />
-        <Route path="/comprar/seleccion-garrafon" element={<SeleccionDeGarrafon />} />
-        <Route path="/comprar/seleccionar-tamano" element={<AguaPuraSeleccionTamano />} />
-        <Route path="/comprar/asignar-garrafones-inicial" element={<AguaPuraPaso2AsignarGarrafonesInicial />} />
-        <Route path="/comprar/asignar-garrafones" element={<AguaPuraPaso2AsignarGarrafones />} />
-        <Route path="/comprar/metodo-entrega" element={<AguaPuraPaso3MetodoEntrega />} />
-        <Route path="/comprar/resumen" element={<AguaPuraResumenPago />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<h1>Bienvenido a AguaPura</h1>} />
+      <Route path="/comprar" element={<Seleccion />} />
+      <Route path="/comprar/seleccion-garrafon" element={<SeleccionDeGarrafon />} />
+      <Route path="/comprar/seleccionar-tamano" element={<AguaPuraSeleccionTamano />} />
+      <Route path="/comprar/asignar-garrafones-inicial" element={<AguaPuraPaso2AsignarGarrafonesInicial />} />
+      <Route path="/comprar/asignar-garrafones" element={<AguaPuraPaso2AsignarGarrafones />} />
+      <Route path="/comprar/metodo-entrega" element={<AguaPuraPaso3MetodoEntrega />} />
+      <Route path="/comprar/resumen" element={<AguaPuraResumenPago />} />
+    </Routes>
   );
 }
 
