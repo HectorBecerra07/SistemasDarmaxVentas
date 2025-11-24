01cfedbb6b8359751850da751af6f803d24b7514
Hector Gabriel Becerra Rivera <rivehect5t@gmail.com>
2025-11-24 14:16:10 -0600

    Sistem

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// Importa tus componentes:
-import AguaPuraSeleccionTamano from "./SistemaDeVentas/compraDeGarrafones/AguaPuraSeleccionTamano";
-import AguaPuraPaso2AsignarGarrafones from "./SistemaDeVentas/compraDeGarrafones/AguaPuraPaso2AsignarGarrafones";
-import AguaPuraPaso3MetodoEntrega from "./SistemaDeVentas/compraDeGarrafones/AguaPuraPaso3MetodoEntrega";
-import AguaPuraResumenPago from "./SistemaDeVentas/compraDeGarrafones/AguaPuraResumenPago";
+import AguaPuraPaso2AsignarGarrafones from "./SistemaDeVentas/compraDeGarrafones/AguaPuraPaso2AsignarGarrafones.jsx";
+import AguaPuraSeleccionTamano from "./SistemaDeVentas/compraDeGarrafones/AguaPuraSeleccionTamano.jsx";
+import AguaPuraPaso2AsignarGarrafonesInicial from "./SistemaDeVentas/AguaPuraPaso2AsignarGarrafonesInicial.jsx";
+import AguaPuraPaso3MetodoEntrega from "./SistemaDeVentas/AguaPuraPaso3MetodoEntrega.jsx";
+import AguaPuraResumenPago from "./SistemaDeVentas/AguaPuraResumenPago.jsx";
+import SeleccionDeGarrafon from "./SistemaDeVentas/SeleccionDeGarrafon.jsx";
+import Seleccion from "./SistemaDeVentas/Seleccion.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
-        {/* Paso 1 */}
-        <Route
-          path="/comprar/seleccionar-tamano"
-          element={<AguaPuraSeleccionTamano />}
-        />
-
-        {/* Paso 2 */}
-        <Route
-          path="/comprar/asignar-garrafones"
-          element={<AguaPuraPaso2AsignarGarrafones />}
-        />
-
-        {/* Paso 3 */}
-        <Route
-          path="/comprar/metodo-entrega"
-          element={<AguaPuraPaso3MetodoEntrega />}
-        />
-
-        {/* Resumen y Pago */}
-        <Route
-          path="/comprar/resumen"
-          element={<AguaPuraResumenPago />}
-        />
-
-        {/* Inicio opcional */}
         <Route path="/" element={<h1>Bienvenido a AguaPura</h1>} />
-
+        <Route path="/comprar" element={<Seleccion />} />
+        <Route path="/comprar/seleccion-garrafon" element={<SeleccionDeGarrafon />} />
+        <Route path="/comprar/seleccionar-tamano" element={<AguaPuraSeleccionTamano />} />
+        <Route path="/comprar/asignar-garrafones-inicial" element={<AguaPuraPaso2AsignarGarrafonesInicial />} />
+        <Route path="/comprar/asignar-garrafones" element={<AguaPuraPaso2AsignarGarrafones />} />
+        <Route path="/comprar/metodo-entrega" element={<AguaPuraPaso3MetodoEntrega />} />
+        <Route path="/comprar/resumen" element={<AguaPuraResumenPago />} />
       </Routes>
     </BrowserRouter>
   );
