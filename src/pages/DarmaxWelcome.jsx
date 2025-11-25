d7e9e54b1d6420e77e8293c7bb76ba7da9be09d1
Hector Gabriel Becerra Rivera <rivehect5t@gmail.com>
2025-11-25 13:41:59 -0600

    ventas

--- src/pages/DarmaxWelcome.jsx
+++ src/pages/DarmaxWelcome.jsx
@@ -27,7 +27,7 @@ const DarmaxWelcome = () => {
 
           {/* Título */}
           <h1 className="text-dark dark:text-white tracking-tight text-3xl font-bold leading-tight text-center pb-2 pt-8 sm:text-4xl">
-           DARMAX
+            Bienvenido a DARMAX super sistema de ventas 
           </h1>
 
           {/* Descripción */}
